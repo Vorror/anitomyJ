88186173d05bca0c8466071ae1e8a6d0fcdf2f20
Paul Miller <miller874@gmail.com>
2016-08-17 00:49:28 -0400

    release

--- cd/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_5c63f50e119b_key -iv $encrypted_5c63f50e119b_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import codesigning.asc
-fi
